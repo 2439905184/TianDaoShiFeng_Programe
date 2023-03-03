aacf5f3a136ebfef00dd8ebae368f0b78f6f905e
小沙盒工作室-室长 <29478722+2439905184@users.noreply.github.com>
2023-03-03 14:35:51 +0800

    Add files via upload

---
+++ 早苗侍奉系统.py
@@ -0,0 +1,133 @@
+import time
+# 乾宫
+def 乾为天():
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+
+def 天风姤():
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+
+def 天山遁():
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+
+def 天地否():
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+
+def 风地观():
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+
+def 山地剥():
+    print("创艺的侍奉：早苗信仰+1")
+
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    
+
+def 火地晋():
+    print("创艺的侍奉：早苗信仰+1")
+
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    
+    print("创艺的侍奉：早苗信仰+1")
+    
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+
+def 火天大有():
+    print("创艺的侍奉：早苗信仰+1")
+
+    print("创艺的侍奉：早苗信仰+1")
+    time.sleep(2)
+    print("创艺的侍奉：早苗信仰+1")
+    
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+    print("创艺的侍奉：早苗信仰+1")
+
+def 侍奉():
+
+    pass
